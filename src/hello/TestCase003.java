cb021d0b0b6fe0d6464a301b80180eb76d799879
Latif Kaya <latif.kayaa@outlook.com>
2022-04-16 02:09:06 +0600

    Creating conflict in TestCase003 in master branch

---
+++ src/hello/TestCase003.java
@@ -0,0 +1,10 @@
+package hello;
+
+public class TestCase003 {
+
+    //this is unknown changes to feature branch
+    //that exists in master
+    //now git will not know what to with this when we merge
+    //conflict will happen.
+
+}
