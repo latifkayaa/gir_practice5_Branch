fb92200a7b8c330dd225a2b330768f2d730841e3
Latif Kaya <latif.kayaa@outlook.com>
2022-04-15 20:49:39 +0600

    some minor changes

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -5,6 +5,10 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello");
+        System.out.println("Hi");
+        System.out.println("Hey there");
+        System.out.println("Hi there");
+
 
     }
 }
