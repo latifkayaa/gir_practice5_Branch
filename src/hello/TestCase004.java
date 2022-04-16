8682484ebde23660a49620e3f2e192b280050254
Latif Kaya <latif.kayaa@outlook.com>
2022-04-16 20:31:17 +0600

    TestCase004 successful

--- src/hello/TestCase004.java
+++ src/hello/TestCase004.java
@@ -4,6 +4,7 @@ public class TestCase004 {
     public static void main(String[] args) {
         System.out.println("Test started.");
         System.out.println("Test in process.");
+        System.out.println("Test done.");
 
     }
 }
