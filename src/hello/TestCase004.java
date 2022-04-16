9b39951922ba106c83f03e2d12740ee9e3fe5227
Latif Kaya <latif.kayaa@outlook.com>
2022-04-16 20:31:04 +0600

    TestCase004 in process

--- src/hello/TestCase004.java
+++ src/hello/TestCase004.java
@@ -3,6 +3,7 @@ package hello;
 public class TestCase004 {
     public static void main(String[] args) {
         System.out.println("Test started.");
+        System.out.println("Test in process.");
 
     }
 }
